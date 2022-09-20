6e57ab95e3d9cf05b085c937eb61debfb6bb5f6a
LIMON029 <limon00029@gmail.com>
2022-09-21 00:19:58 +0900

    map api updated

--- repository/map.repository.js
+++ repository/map.repository.js
@@ -1,8 +1,84 @@
 const model = require("../models");
 const { Op } = require("sequelize");
 const sequelize = model.sequelize;
-const moment = require("moment");
 
 const Map = {}
+
+Map.createMap = async (map, resultFunc) => {
+    let createdMap;
+    try {
+        await sequelize.transaction(async trans => {
+            createdMap = await model.Map.create({
+                map_info: map.map_info,
+                map_tag: map.map_tag != null ? map.map_tag : "none",
+                map_grade: map.map_grade != null ? map.map_grade : 0,
+                map_difficulty: map.map_difficulty != null ? map.map_grade : 0,
+                map_maker: map.map_maker != null ? map.map_maker : "unknown"
+            }, {transaction: trans});
+        })
+    } catch(err) {
+        console.log("createMap err", err);
+        return resultFunc(err, null);
+    }
+    console.log("Map is created.");
+    return resultFunc(err, createdMap);
+}
+
+Map.findAllMaps = (resultFunc) => {
+    model.Map.findAll({
+        raw: true,
+        where: {},
+        attributes: ['map_id', 'map_info', 'map_tag', 'map_grade', 'map_difficulty', 'map_maker']
+    }).then((result) => {
+        console.log("Find All Maps");
+        return resultFunc(null, result);
+    }).catch((err) => {
+        console.log("findAllMaps err", err);
+        return results(err, null);
+    });
+}
+
+Map.findAllMapsByTag = (map_tag, resultFunc) => {
+    model.Map.findAll({
+        raw: true,
+        where: {map_tag: map_tag},
+        attributes: ['map_id', 'map_info', 'map_tag', 'map_grade', 'map_difficulty', 'map_maker']
+    }).then((result) => {
+        console.log(`Find all maps with map_tag(${map_tag}).`);
+        return resultFunc(null, result);
+    }).catch((err) => {
+        console.log("findAllMapsByTag err", err);
+        return results(err, null);
+    });
+}
+
+Map.updateMapInfo = async (forUpdate, resultFunc) => {
+    try {
+        await sequelize.transaction(async trans => {
+            await model.Map.update(
+                forUpdate,
+                {}
+            )
+        })
+    } catch(err) {
+
+    }
+}
+
+Map.deleteMap = async (map_id, resultFunc) => {
+    try {
+        await sequelize.transaction(async trans => {
+            await model.Map.destroy({
+                where: {map_id: map_id}
+            });
+        });
+    } catch(err) {
+        console.log("deleteMap err", err);
+        return resultFunc(err, null);
+    }
+
+    console.log(`Map(${map_id}) is deleted.`);
+    return resultFunc(null, "done");
+}
 
 module.exports = Map;
--- service/map.service.js
+++ service/map.service.js
@@ -1,2 +1,75 @@
 const map = require("../repository/map.repository.js");
 
+exports.applyMap = (req, res) => {
+    if(!req.body){
+        res.status(400).send({
+            message: "req.body can not be empty!"
+        });
+    } else {
+        map.createMap(req.body, (err, result) => {
+            if(err) {
+                res.status(400).send({
+                    message: "applyMap err"
+                });
+            } else {
+                res.send({
+                    map_id: result.map_id
+                });
+            }
+        });
+    }
+}
+
+exports.getAllMapList = (req, res) => {
+    map.findAllMaps((err, results) => {
+        if (err) {
+            res.status(400).send({
+                message: "getAllMapList err"
+            });
+        } else {
+            res.send(results);
+        }
+    })
+}
+
+exports.getMapListByTag = (req, res) => {
+    if(!req.params){
+        res.status(400).send({
+            message: "req.body can not be empty!"
+        });
+    } else {
+        map.findAllMapsByTag(req.params.map_tag, (err, result) => {
+            if(err) {
+                res.status(400).send({
+                    message: "getMapListByTag err"
+                });
+            } else {
+                res.send({result: result, map_tag: req.params.map_tag});
+            }
+        });
+    }
+}
+
+exports.deleteMap = (req, res) => {
+    if (!req.params) {
+        res.status(400).send({
+            message: "req.params can not be empty!"
+        });
+    } else if (!req.user) {
+        res.status(401).send({
+            message: "Unauthorized"
+        });
+    } else {
+        map.deleteMap(req.params.map_id, (err, results) => {
+            if(err) {
+                res.status(400).send({
+                    message: "deleteMap err"
+                });
+            } else {
+                res.send({
+                    message: `map(${req.params.map_id}) was successfully deleted`
+                });
+            }
+        })
+    }
+}
