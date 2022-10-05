b826c2f56804b7123ebe29e79cbf6cfb009b31f6
limon029 <limon00029@gmail.com>
2022-10-05 21:00:19 +0900

    fix config.js for mariadb connection

--- config/config.js
+++ config/config.js
@@ -5,6 +5,9 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database:process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
     dialect:"mariadb",
 };
+
+// port: process.env.DB_PORT 삭제
+// mariadb의 경우 port: process.env.DB_PORT를 적지 않음
+// 포트포워딩 문제로 예상됨
