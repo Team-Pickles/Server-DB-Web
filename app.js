ae99e5e9afaa8e8a6b243c64cd292dd4ecd9a68b
limon029 <limon00029@gmail.com>
2022-11-27 20:30:30 +0900

    Extend body limit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { sequelize } = require("./models");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json({limit: '50mb', extended: true}))
 app.use(express.urlencoded({extended:true}));
 app.use( cors({ 
     origin: "*", 
